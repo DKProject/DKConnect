543fdc3d3b20b211b12614e1db25aad693e23a44
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2022-08-11 19:24:53 +0200

    Update DKConnectPlugin.java

--- dkconnect-minecraft/src/main/java/net/pretronic/dkconnect/minecraft/DKConnectPlugin.java
+++ dkconnect-minecraft/src/main/java/net/pretronic/dkconnect/minecraft/DKConnectPlugin.java
@@ -40,9 +40,6 @@ import java.util.concurrent.TimeUnit;
 
 public class DKConnectPlugin extends MinecraftPlugin {
 
-    public static final String RESOURCE_ID = "1e6d4f31-e2e0-11eb-8ba0-0242ac180002";
-    public static final String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoTmvCUx+BvHKWeyCcjpOC8lbe8Pe/1ERyYu5/aSfj1qY2gEa/ie3u3c+ejgiZ5IBckR4qKmRkrBNQCtmd7ojinB7WFqM1xvdyWkK/s3Vv0tzBgZwqXTjX6W07WRFrp/oW1JvA8aShdoVqDVxlWYTkd6mKTTBEs9vKfeTn4GuQExG+qXV7gYm3WAMGehQ3YL3zxGGxjV78HMLyNrK7RbmX0AocDTpDoJeUNT3RDHS9kHQHSUYhHup3XjhymWeiVSh4cb8R5IF61uuxQdpE9KtXuT4qSCgHPUQyoRSuJq6zflCIJngSoTF09eDS0wTeICNvt0WMVpBexDmYJN8owwIgwIDAQAB";
-
     private DefaultDKConnect dkConnect;
     private Collection<DiscordGuildConfig> guildConfigs;
 
@@ -50,17 +47,6 @@ public class DKConnectPlugin extends MinecraftPlugin {
     public void onLoad(LifecycleState state) {
         getLogger().info("DKConnect is starting, please wait..");
         //SLF4JStaticBridge.setLogger(getLogger());
-        try{
-            McNativeLicenseIntegration.newContext(this,RESOURCE_ID,PUBLIC_KEY).verifyOrCheckout();
-        }catch (LicenseNotValidException | CloudNotCheckoutLicenseException e){
-            getLogger().error("--------------------------------");
-            getLogger().error("-> Invalid license");
-            getLogger().error("-> Error: "+e.getMessage());
-            getLogger().error("--------------------------------");
-            getLogger().info("DKConnect is shutting down");
-            getLoader().shutdown();
-            return;
-        }
 
         DefaultDKConnect dkConnect = new DefaultDKConnect(getRuntime().getLocal().getEventBus(), getLogger(), getDatabaseOrCreate(),
                 playerId -> McNative.getInstance().getPlayerManager().getPlayer(playerId).getLanguage());
